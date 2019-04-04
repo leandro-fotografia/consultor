daa969fe0b65f29d245300d59d65b950f2998f6d
Jefferson <jefferson.santana@unicid.edu.br>
2019-04-04 17:23:07 -0300

    target

--- wordpress/wp-content/themes/leandro-consultor/header.php
+++ wordpress/wp-content/themes/leandro-consultor/header.php
@@ -56,7 +56,7 @@
 					</ul>
 					<div class="buttons">
 						<p>
-							<a class="text-white" href="https://www.linkedin.com/in/leandrolnogueira/"><i class="fab fa-linkedin-in"></i></a>
+							<a class="text-white" href="https://www.linkedin.com/in/leandrolnogueira/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
 							<i class="fas mr-1 fa-phone"></i> <?=getTelefone(get_the_ID());?>
 						</p>	
 					</div>
